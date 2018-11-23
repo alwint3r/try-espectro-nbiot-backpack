3c8f790d8846989b6c1232d41611147edd8f67e8
Alwin Arrasyid <alwin.ridd@gmail.com>
2018-11-23 19:43:29 +0700

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+#include <Arduino.h>
+#include <HardwareSerial.h>
+#include <Sodaq_nbIOT.h>
+#include <Sodaq_nbIOT_UDP.h>
+
+HardwareSerial nbSerial(1);
+
+#define PIN_ENABLE 17
+#define RXPIN 25
+#define TXPIN 26
+#define DTRPIN 14
+#define RTSPIN 2
+
+Sodaq_nbIOT nbiot;
+Sodaq_nbIOT_UDP udp(nbiot);
+
+void setup() {
+    Serial.begin(115200);
+
+    pinMode(DTRPIN, OUTPUT);
+    pinMode(RTSPIN, OUTPUT);
+    digitalWrite(DTRPIN, LOW);
+    digitalWrite(RTSPIN, LOW);
+
+    nbSerial.begin(115200, SERIAL_8N1, RXPIN, TXPIN);
+
+    nbiot.init(nbSerial, -1, -1, -1, 1);
+    nbiot.setDiag(Serial);
+    nbiot.on();
+    nbiot.connect("iotxl", "");
+}
+
+void loop() {
+    while (Serial.available()) {
+        nbSerial.write(Serial.read());
+    }
+
+    while (nbSerial.available()) {
+        Serial.write(nbSerial.read());
+    }
+}
